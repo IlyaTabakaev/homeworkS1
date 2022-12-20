59d42c8ff74df9f5fd4971afe4d15aa6b984f5ed
Ilya <osetia9595@gmail.com>
2022-12-20 21:08:50 +0300

    tsk1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите чило A");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите чило B");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > numberB) max = numberA;
+if (numberA < numberB) max = numberB;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
