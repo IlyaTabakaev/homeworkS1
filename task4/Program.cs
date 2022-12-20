388df688fd3f67471da15b35748f4a50ff71094a
Ilya <osetia9595@gmail.com>
2022-12-20 21:11:56 +0300

    tsk4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int numberA = int.Parse(Console.ReadLine());
+
+int count = 1;
+
+while (count <= numberA)
+{
+    if (count % 2 == 0)
+    {
+    Console.Write($"{count}");
+    }
+count ++;
+}
+
+
