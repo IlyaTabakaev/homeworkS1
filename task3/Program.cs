f5be728408665eb2154ed2b7325960041e10f3b2
Ilya <osetia9595@gmail.com>
2022-12-20 21:10:57 +0300

    tsk3

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int numberA = int.Parse(Console.ReadLine());
+
+
+
+ if (numberA % 2 == 0)
+ {
+    Console.WriteLine("четное");
+ }
+
+else
+{
+    Console.WriteLine("нечетное");
+}
